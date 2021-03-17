ac67450524b8d1f7add5309ba8b9a4c754483de4
Saiteja.Segu <saiteja.segu@ibm.com>
2021-03-17 16:48:09 +0530

    Auth Completed

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,9 +9,15 @@ import { NgAuthService } from "../../ng-auth.service";
 
 export class DashboardComponent implements OnInit {
 
+  user:any;
+  parsedJson: any;
   constructor(public ngAuthService: NgAuthService) { }
 
   ngOnInit(): void {
+    this.user = localStorage.getItem('user');
+    this.parsedJson = JSON.parse(this.user);
+    console.log(this.parsedJson.uid);
+    localStorage.setItem('authtoken',this.parsedJson.uid);
   }
 
 }
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -15,4 +15,5 @@ export class SignInComponent implements OnInit {
 
   ngOnInit() { }
 
+
 }
